c741842adc59ffc35d9df40577a20fcecf830e3d
JurgenKV <yura.kireev.vt@gmail.com>
2023-08-25 18:20:01 +0300

    Added data logic

---
+++ Assets/Scripts/Data/QuestionsList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "QuestionsList", menuName = "QuestionsList")]
+public class QuestionsList : ScriptableObject
+{
+    [SerializeField] public List<Person> Persons;
+    [SerializeField] public List<QuestionItem> QuestionItems;
+
+    public void NewGame()
+    {
+        foreach (Person person in Persons)
+        {
+            person.Score = 0;
+        }
+    }
+}
+
+[Serializable]
+public class Person
+{
+    public int Id;
+    public string Type;
+    public string Description;
+    public int Score;
+}
+
+[Serializable]
+public class QuestionItem
+{
+    public int Id;
+    public string QuestionText;
+    
+    public string VariantFirst;
+    public int PersonIdFirst;
+    
+    public string VariantSecond;
+    public int PersonIdSecond;
+}
+
