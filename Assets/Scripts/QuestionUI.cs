899e2fe1536da12d881f13593c14b0f715750baf
JurgenKV <yura.kireev.vt@gmail.com>
2023-08-29 13:49:06 +0300

    Added bg variants

--- Assets/Scripts/QuestionUI.cs
+++ Assets/Scripts/QuestionUI.cs
@@ -1,15 +1,25 @@
 ﻿
+    using System;
+    using System.Collections.Generic;
     using TMPro;
     using UnityEngine;
+    using Random = UnityEngine.Random;
 
     public class QuestionUI : MonoBehaviour
     {
         [SerializeField] private TMP_Text _text;
         [SerializeField] private TMP_Text _textVariantFirst;
         [SerializeField] private TMP_Text _textVariantSecond;
-
+        [SerializeField] private List<GameObject> _bgVariants;
         private QuestionItem _questionItem;
+
+        private void Awake()
+        {
+            RoleBgVariant();
+        }
         
+        
+
         public void CreateUI(QuestionItem questionItem)
         {
             _questionItem = questionItem;
@@ -21,10 +31,18 @@
         public void ButtonFirst()
         {
             FindObjectOfType<GameController>().HandleButtonVariantFirst(_questionItem);
+            RoleBgVariant();
         }
 
         public void ButtonSecond()
         {
             FindObjectOfType<GameController>().HandleButtonVariantSecond(_questionItem);
+            RoleBgVariant();
+        }
+
+        private void RoleBgVariant()
+        {
+            _bgVariants.ForEach(i=>i.SetActive(false));
+            _bgVariants[Random.Range(0, _bgVariants.Count)].SetActive(true);
         }
     }
