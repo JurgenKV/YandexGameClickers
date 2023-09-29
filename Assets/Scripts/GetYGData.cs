7381d5715bfdf2348cfca5f889e3038fbe713516
JurgenKV <yura.kireev.vt@gmail.com>
2023-09-29 17:17:42 +0300

    Added start anim

--- Assets/Scripts/GetYGData.cs
+++ Assets/Scripts/GetYGData.cs
@@ -7,13 +7,32 @@
     public class GetYGData : MonoBehaviour
     {
         [SerializeField] private string _sceneName;
+        [SerializeField] private Animator _animator;
         
         private void OnEnable() => YandexGame.GetDataEvent += GetData;
-
+        
+        private void OnDisable() => YandexGame.GetDataEvent -= GetData;
+        
+        private void Awake()
+        {
+            if (YandexGame.SDKEnabled == true)
+            {
+                GetData();
+                Debug.Log("SDKEnabled == true");
+            }
+        }
+        
         private void GetData()
         {
             Debug.Log(YandexGame.savesData.MoneyAmount + "Score");
-            SceneManager.LoadScene(_sceneName);
+            _animator.SetTrigger("AnimEnd");
+            Invoke(nameof(LoadGameEvent), 1);
         }
         
+
+        public void LoadGameEvent()
+        {
+            SceneManager.LoadScene(_sceneName);
+        }
+
     }
